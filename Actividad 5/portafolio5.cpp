771ba559443fbb996785d6fa4d60fc5c159c55b8
Dylan Melendez <00087018@uca.edu.sv>
2019-09-13 07:17:13 -0600

    Actividad 5

--- Actividad 5/portafolio5.cpp
+++ Actividad 5/portafolio5.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int multiplicacion(int num1, int num2){
-	if(num1==1||num2==1){
+	if(num1==0||num2==0){
+		return 0;
+	}else if(num1==1||num2==1){
 		cout << "n = " << num2 << " caso base" << endl;
 		return 1;
 	}else{
-		int a = num1;
-		int b = num1*(num2-1);
-		cout << "n=" << num1 << " a=" << a <<" b=" << b << endl;
-		return a+b;	
+		multiplicacion(num1, num2-1);
+		return num1+(num1*(num2-1));
 	}
 }
 
